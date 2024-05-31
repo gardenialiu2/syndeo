3b37a42180adb971887018e1a47bfe6c487ba7ed
Yuen Ler Chow <lerchow@gmail.com>
2024-05-30 22:11:35 -0500

    change

--- client/src/Refer.tsx
+++ client/src/Refer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
